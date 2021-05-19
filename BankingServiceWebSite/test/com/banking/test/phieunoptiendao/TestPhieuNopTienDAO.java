6846b846c41a7d0b4766c377cde7727ef764c7cc
tblue160397 <64902763+tblue160397@users.noreply.github.com>
2021-05-19 09:05:07 +0700

    add Junit

--- BankingServiceWebSite/test/com/banking/test/phieunoptiendao/TestPhieuNopTienDAO.java
+++ BankingServiceWebSite/test/com/banking/test/phieunoptiendao/TestPhieuNopTienDAO.java
@@ -1,24 +1,57 @@
-//package com.banking.test.phieunoptiendao;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import java.sql.Connection;
-//import java.sql.Date;
-//import java.sql.SQLException;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import com.banking.dao.UsersDAO;
-//import com.banking.entity.hopDongVay;
-//import com.banking.entity.phieunoptien;
-//
-//class TestThemPhieuNop {
-//
+package com.banking.phieunoptiendao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import com.banking.dao.PhieuNopTienDAO;
+import com.banking.dao.UsersDAO;
+import com.banking.entity.hopDongVay;
+import com.banking.entity.phieunoptien;
+
+class TestThemPhieuNop {
+
+	@Test
+	void test_Success_1() throws ClassNotFoundException, SQLException {
+		boolean kqTest= true;
+		
+		PhieuNopTienDAO pntDAO = new PhieuNopTienDAO();
+		
+		Connection connection = pntDAO.getDAOConnection();
+		connection.setAutoCommit(false);
+		
+		try {
+			phieunoptien pnt = new phieunoptien();
+			hopDongVay hdv = new hopDongVay();
+			long millis=System.currentTimeMillis();  
+			Date now = new Date(millis);
+			hdv.setID(10);
+			pnt.setHopDongVay(hdv);
+			pnt.setNgayDong(now);
+			pnt.setTienLai((float)2484.8398);
+			pnt.setTienGoc((float)13804.667);
+			pnt.setTimeID(1);
+			
+			boolean kqDB = pntDAO.themPhieuNop(pnt);
+			assertEquals(kqTest, kqDB);			
+		} finally {
+			// TODO: handle finally clause
+//			connection.rollback();
+			connection.close();
+		}
+	}
+	
 //	@Test
-//	void test_Success() throws ClassNotFoundException, SQLException {
+//	void test_Success_2() throws ClassNotFoundException, SQLException {
 //		boolean kqTest= true;
 //		
-//		Connection connection = UsersDAO.getDAOConnection();
+//		PhieuNopTienDAO pntDAO = new PhieuNopTienDAO();
+//		
+//		Connection connection = pntDAO.getDAOConnection();
 //		connection.setAutoCommit(false);
 //		
 //		try {
@@ -26,16 +59,80 @@
 //			hopDongVay hdv = new hopDongVay();
 //			long millis=System.currentTimeMillis();  
 //			Date now = new Date(millis);
-//			hdv.setID(10);
+//			hdv.setID(14);
 //			pnt.setHopDongVay(hdv);
 //			pnt.setNgayDong(now);
-//			pnt.setTienLai((float)1);
-//			pnt.setTienGoc((float)0);
+//			pnt.setTienLai((float)139.86);
+//			pnt.setTienGoc((float)777.0);
 //			pnt.setTimeID(1);
 //			
+//			boolean kqDB = pntDAO.themPhieuNop(pnt);
+//			assertEquals(kqTest, kqDB);			
 //		} finally {
 //			// TODO: handle finally clause
+////			connection.rollback();
+////			connection.close();
 //		}
 //	}
-//
-//}
+	
+//	@Test
+//	void test_Fail_1() throws ClassNotFoundException, SQLException {
+//		boolean kqTest= false;
+//		
+//		PhieuNopTienDAO pntDAO = new PhieuNopTienDAO();
+//		
+//		Connection connection = pntDAO.getDAOConnection();
+//		connection.setAutoCommit(false);
+//		
+//		try {
+//			phieunoptien pnt = new phieunoptien();
+//			hopDongVay hdv = new hopDongVay();
+//			long millis=System.currentTimeMillis();  
+//			Date now = new Date(millis);
+//			hdv.setID(28);
+//			pnt.setHopDongVay(hdv);
+//			pnt.setNgayDong(now);
+//			pnt.setTienLai((float)2484.8398);
+//			pnt.setTienGoc((float)13804.667);
+//			pnt.setTimeID(1);
+//			
+//			boolean kqDB = pntDAO.themPhieuNop(pnt);
+//			assertEquals(kqTest, kqDB);			
+//		} finally {
+//			// TODO: handle finally clause
+//			connection.rollback();
+////			connection.close();
+//		}
+//	}
+//	
+//	@Test
+//	void test_Fail_2() throws ClassNotFoundException, SQLException {
+//		boolean kqTest= false;
+//		
+//		PhieuNopTienDAO pntDAO = new PhieuNopTienDAO();
+//		
+//		Connection connection = pntDAO.getDAOConnection();
+//		connection.setAutoCommit(false);
+//		
+//		try {
+//			phieunoptien pnt = new phieunoptien();
+//			hopDongVay hdv = new hopDongVay();
+//			long millis=System.currentTimeMillis();  
+//			Date now = new Date(millis);
+//			hdv.setID(29);
+//			pnt.setHopDongVay(hdv);
+//			pnt.setNgayDong(now);
+//			pnt.setTienLai((float)2484.8398);
+//			pnt.setTienGoc((float)13804.667);
+//			pnt.setTimeID(1);
+//			
+//			boolean kqDB = pntDAO.themPhieuNop(pnt);
+//			assertEquals(kqTest, kqDB);			
+//		} finally {
+//			// TODO: handle finally clause
+//			connection.rollback();
+////			connection.close();
+//		}
+//	}
+
+}
