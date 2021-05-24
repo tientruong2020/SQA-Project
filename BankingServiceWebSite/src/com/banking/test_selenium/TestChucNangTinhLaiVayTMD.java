6211c8b5c7c30fafbb30dd285a8464c9b3abb5d7
ThinhMD <ThinhMDITPTIT@gmail.com>
2021-05-25 00:32:49 +0700

    Change file test name - ThinhMD

--- BankingServiceWebSite/src/com/banking/test_selenium/TestChucNangTinhLaiVayTMD.java
+++ BankingServiceWebSite/src/com/banking/test_selenium/TestChucNangTinhLaiVayTMD.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class TestTitleDangNhap extends TestDriver {
+public class TestChucNangTinhLaiVayTMD extends TestDriver {
 
 	ChromeDriver driver = getDriver();
 	@Test
